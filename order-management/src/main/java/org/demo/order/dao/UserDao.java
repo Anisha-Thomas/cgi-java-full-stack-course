3e7ab0bee563d1cd0685dc4f2a556725a657a424
anisha-thomas-100 <131661251+anisha-thomas-100@users.noreply.github.com>
2023-06-02 07:51:20 +0530

    Delete order-management directory

--- order-management/src/main/java/org/demo/order/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package org.demo.order.dao;
-
-import org.demo.order.model.User;
-
-public interface UserDao {
-
-	User getUserVerified(String uname, String password);
-
-}
--- order-management/src/main/java/org/demo/order/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package org.demo.order.service;
-
-import org.demo.order.model.User;
-
-public interface UserService {
-
-	public User getUserVerified(String uname, String password);
-}
