56aa4f12c3f7751b78bfac3b6a2cd7f894c1582a
anisha-thomas-100 <131661251+anisha-thomas-100@users.noreply.github.com>
2023-06-09 20:37:09 +0530

    Spring Rest API with password encrypted

---
+++ user-service/src/main/java/com/example/demo/dto/UserDto.java
@@ -0,0 +1,75 @@
+package com.example.demo.dto;
+
+public class UserDto {
+	private String userId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private String encrytedPassword;
+	public UserDto(String userId,String firstName, String lastName, String email, String password,
+			String encrytedPassword) {
+		this.userId=userId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.encrytedPassword = encrytedPassword;
+	}
+	
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public UserDto() {
+		super();
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEncrytedPassword() {
+		return encrytedPassword;
+	}
+
+	public void setEncrytedPassword(String encrytedPassword) {
+		this.encrytedPassword = encrytedPassword;
+	}
+
+	@Override
+	public String toString() {
+		return "UserDto [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", password="
+				+ password + ", encrytedPassword=" + encrytedPassword + "]";
+	}
+
+
+	
+	
+	
+}
---
+++ user-service/src/main/java/com/example/demo/ui/UserRequestModel.java
@@ -0,0 +1,53 @@
+package com.example.demo.ui;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserRequestModel {
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	
+
+}
